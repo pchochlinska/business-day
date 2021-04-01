8b47abee209a410d3f5eca3d5f1b5f9e6c652a18
Patrycja Chochlińska <52857432+pchochlinska@users.noreply.github.com>
2021-04-01 13:14:34 +0200

    Fixed pl holidays names

--- src/Cmixin/HolidayNames/pl.php
+++ src/Cmixin/HolidayNames/pl.php
@@ -1,19 +1,18 @@
 <?php
 
 return [
-    '05-01'              => 'Święto Państwowe; Święto Pracy',
-    'constitution-day'   => 'Święto Narodowe Trzeciego Maja',
-    '05-03'              => 'Święto Narodowe Trzeciego Maja',
     'new-year'           => 'Nowy Rok',
     '01-06'              => 'Święto Trzech Króli',
+    'easter'             => 'Wielkanoc',
+    'easter-1'           => 'Poniedziałek Wielkanocny',
+    '05-01'              => 'Święto Pracy',
+    'constitution-day'   => 'Święto Konstytucji Trzeciego Maja',
+    '05-03'              => 'Święto Konstytucji Trzeciego Maja',
+    'easter-49'          => 'Zielone Świątki',
+    'easter-60'          => 'Boże Ciało',
     '08-15'              => 'Wniebowzięcie Najświętszej Maryi Panny',
     '11-01'              => 'Wszystkich Świętych',
-    'christmas'          => 'Pierwszy dzień Bożego Narodzenia',
+    'independence-day'   => 'Święto Niepodległości',
+    'christmas'          => 'Boże Narodzenie',
     'christmas-next-day' => 'Drugi dzień Bożego Narodzenia',
-    'easter'             => 'Niedziela Wielkanocna',
-    'easter-1'           => 'Drugi dzień Wielkanocy',
-    'easter-49'          => 'Zielone Świątki',
-    'easter-60'          => 'Dzień Bożego Ciała',
-    'independence-day'   => 'Narodowe Święto Niepodległości',
-    'mothers-day'        => 'Dzień Matki',
 ];
